168c348b84c103890dc0ea54994a36e27da17c38
Saori Kohno <saorikohno@yahoo.com>
2016-01-27 16:37:20 +0900

    Initial Commit

---
+++ Sao's PDF Kit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Sao's PDF Kit
+//
+//  Created by 河野 さおり on 2016/01/27.
+//  Copyright © 2016年 河野 さおり. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Sao's PDF Kit/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Sao's PDF Kit
+//
+//  Created by 河野 さおり on 2016/01/27.
+//  Copyright © 2016年 河野 さおり. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
